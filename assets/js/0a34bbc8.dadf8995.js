aaaa7d4c0d89d9caf72b6563001ef94c4d756b6e
Pascal Witzig <pascal.witzig@gmail.com>
2022-05-21 23:14:41 +0200

    Deploy website - based on a9c1f377aa466e584371bf023f9c5d394a802cc9

--- assets/js/0a34bbc8.dadf8995.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([[119],{9716:function(e){e.exports=JSON.parse('{"title":"HTML","description":"5 minutes to learn the most important Docusaurus concepts.","slug":"/category/html","permalink":"/docs/category/html","navigation":{"next":{"title":"Create a Page","permalink":"/docs/frontend/HTML/create-a-page"}}}')}}]);
